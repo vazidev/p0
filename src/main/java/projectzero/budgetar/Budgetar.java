a85ad27ca340e7cd8573593f65f90438c896440e
VaziDev <Nganga25@Gmail.com>
2021-07-27 07:59:29 -0700

    New Branch Sort

--- src/main/java/projectzero/budgetar/Budgetar.java
+++ src/main/java/projectzero/budgetar/Budgetar.java
@@ -7,11 +7,10 @@ public class Budgetar {
         System.out.println("Create Data File"); //working
         String a = new BudgetarFile().CreateDataFile("cogsFile.csv");  //Make sure the Data  File Exists : Create : Working
         double b =  new GetData().GetBudget(0);         // Get the Budget Amount : Working
-       // HashMap c = new GetData().GetShoppingList();  // Collect the Items Cost Data  and store in file :Working
+       HashMap c = new GetData().GetShoppingList();  // Collect the Items Cost Data  and store in file :Working
         String[] d =  new BudgetarFile().ReadDataFile("cogsFile.csv");  //read Current List stored in File : Working
         HashMap e = new GetData().IndexShoppingList();     //Edit the Items Necessity Index (NV).
 
-        System.out.println("Store Shopping List to File");
 
         new ShoppingList();
 
